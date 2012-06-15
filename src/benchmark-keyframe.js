1c0c86bb53af2dfb53c4d18337f495f4ec192f53
Jeremy Kahn <jeremyckahn@gmail.com>
2012-06-14 20:14:58 -0700

    Adds r.js and gets things to load.

---
+++ src/benchmark-keyframe.js
@@ -0,0 +1,12 @@
+var r = require('../lib/r.js');
+r.config({
+  paths: {
+    shifty: '../lib/shifty',
+    underscore: '../lib/underscore',
+    rekapi: '../lib/rekapi'
+  }
+});
+
+r(['rekapi'], function(Kapi) {
+  var kapi = new Kapi();
+});
