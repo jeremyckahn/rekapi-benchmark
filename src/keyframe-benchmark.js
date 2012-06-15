2f4421da301500d42f5331bac5ebaa5e299372cf
Jeremy Kahn <jeremyckahn@gmail.com>
2012-06-14 21:02:56 -0700

    Builds out the Keyframe benchmark.

--- src/keyframe-benchmark.js
+++ src/keyframe-benchmark.js
@@ -1,3 +1,23 @@
+function now () {
+  return +(new Date());
+}
+
+var TIMES_TO_ADD_KEYFRAMES = 200;
+
 module.exports = function (Kapi) {
-  console.log(Kapi);
+  var kapi = new Kapi();
+  var actor =  new Kapi.Actor();
+  kapi.addActor(actor);
+  var startTime = now();
+
+  var i;
+  for (i = 0; i < TIMES_TO_ADD_KEYFRAMES; i++) {
+    actor.keyframe(i, { x: 1 });
+  }
+
+  var endTime = now();
+  var timeElapsed = endTime - startTime;
+  console.log('Finished in ' + timeElapsed + ' ms, iterated ' +
+      TIMES_TO_ADD_KEYFRAMES + ' times');
+
 };
